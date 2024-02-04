1772ec857eb407e1cb3136ba453f4d8df7774190
Dmytry <dmtrdats@gmail.com>
2024-02-04 21:13:11 +0200

    axios commit

--- client/src/components/Input/Input.jsx
+++ client/src/components/Input/Input.jsx
@@ -3,7 +3,10 @@ import './input.css'
 
 const Input = (props) => {
     return (
-        <input type={props.type} placeholder={props.placeholder} />
+        <input onChange={(event)=>props.setValue(event.target.value)}
+        value={props.value} 
+        type={props.type} 
+        placeholder={props.placeholder} />
     );
 };
 
--- client/src/components/Registration.jsx
+++ client/src/components/Registration.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import {React, useState} from 'react';
 import './registration.css';
 import Input from './Input/Input';
+import { registration } from '../actions/user';
 
 const Registration = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
     return (
         <div className='registration'>
             <div className="registration__header">Регистрация</div>
-            <Input type="text" placeholder="Enter email"/>
-            <Input type="password" placeholder="Enter password"/>
+            <Input value={email} setValue={setEmail} type="text" placeholder="Enter email"/>
+            <Input value={password} setValue={setPassword} type="password" placeholder="Enter password"/>
             <Input type="password" placeholder="Confirm password"/>
 
-            <button className="registration__btn">Войти</button>
+            <button onClick={() => registration(email, password)} className="registration__btn">Войти</button>
         </div>
     );
 };
